79edd75141a40d624706bb0a7d5f116571b8313a
TommySTYP <tommystyp@gmail.com>
2020-04-25 22:16:54 +0700

    index.php 2

--- index.php
+++ index.php
@@ -22,16 +22,16 @@
 
   <body>
     <form id="login" method="post">
-      <label for="username">Username</label><input type="text" name="username" id="username" />
+      <label for="username">Username : </label><input type="text" name="username" id="username" />
       <br class="clear" /> 
-      <label for="password">password</label><input type="password" name="password" id="password" />
+      <label for="password">Password : </label><input type="password" name="password" id="password" />
       <br class="clear" /> 
       <button type="submit" id="btn-submit">เข้าสู่ระบบ</button>
-    </form>
+    </form><BR><BR>
 
 
-    <button onclick="window.location.href='/demo-pg'">ทดลองเล่น</button><BR>
-    <button onclick="window.location.href='<?=$config['host']?>/register';">สมัครสมาชิก</button><BR>
+    <button onclick="window.location.href='/demo-pg'">ทดลองเล่น</button><BR><BR>
+    <button onclick="window.location.href='<?=$config['host']?>/register';">สมัครสมาชิก</button><BR><BR>
     
     
     <?php
