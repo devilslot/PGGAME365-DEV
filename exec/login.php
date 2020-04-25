7224bf59bff510dfc3a74264f8083b3b955c853c
TommySTYP <tommystyp@gmail.com>
2020-04-25 22:57:03 +0700

    commit

--- exec/login.php
+++ exec/login.php
@@ -1,19 +1,18 @@
 <?php
 require_once '../dbmodel.php';
-require_once '../function.php';
-require_once '../config.php';
+//require_once '../function.php';
+//require_once '../config.php';
 if (isset($_POST['phone']) and isset($_POST['password'])) {
     $phone  = trim($_POST['phone']);
     $password = trim($_POST['password']);
-    $stmt = $mysqli->prepare("SELECT `member_username` FROM `members` WHERE `member_phone` = ? AND `member_password` = ?");
-    $stmt->bind_param("ss", $phone, $password);
-    $stmt->execute();
-    $stmt->store_result();
-    if ($stmt->num_rows > 0) {
+    $sql = "SELECT * FROM members WHERE ";
+    $sql .= "member_phone = '" . $phone . "' AND member_password = '" . $password . "'";
+    $data_members = $mysqli->query($sql)->fetch_assoc();
+    if (count($data_members) > 0) {
         $stmt->bind_result($username); 
         $stmt->fetch();
         $_SESSION['username'] = $username;
-        echo "<script>window.location = 'exec/profile.php'</script>";
+        echo "<script>window.location = '../profile.php'</script>";
     }else{
         echo "<script>Swal.fire(
             'เข้าสู่ระบบไม่สำเร็จ',
