46a3bfbead95c99bd1bf9b2a6820f568a2f90897
TommySTYP <tommystyp@gmail.com>
2020-04-25 21:52:46 +0700

    commit login form

--- index.php
+++ index.php
@@ -21,13 +21,30 @@
   </head>
 
   <body>
+  <form id="login" method="post">
+    <label for="username">Username</label><input type="text" name="username" id="username" />
+    <br class="clear" /> 
+    <label for="password">password</label><input type="password" name="password" id="password" />
+    <br class="clear" /> 
+    <button type="submit" id="btn-submit">เข้าสู่ระบบ</button>
+  </form>
+
+
     <button onclick="window.location.href='/login';">เข้าสู่ระบบ</button><BR>
     <button onclick="window.location.href='/demo-pg'">ทดลองเล่น</button><BR>
     <button onclick="window.location.href='<?=$config['host']?>/register';">สมัครสมาชิก</button><BR>
     <?php
-      echo hash("sha256",rand());
+      //echo hash("sha256",rand());
     ?>
-</body>
+			<script>
+            $("#register").submit(function(e) {
+               e.preventDefault();
+               $.post('/exec/login.php', $(this).serialize(), function(data) {
+                  $("#alerts").html(data)
+               });
+            });
+			</script>
+  </body>
 
 </html>
 
